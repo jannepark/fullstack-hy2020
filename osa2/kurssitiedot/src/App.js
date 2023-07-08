7a388ae2ed6abd27a14c5e36b02ce95d32892e75
jannepark <janneviljo@live.com>
2023-07-08 12:09:21 +0300

    2.1-2.5 done

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,55 +1,58 @@
+import Course from './components/Course'
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
+  const courses = [
     {
-      name: 'Fundamentals of React',exercises: 10
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
     },
     {
-      name: 'Using props to pass data',exercises: 7
-    },
-    {
-      name: 'State of a component',exercises: 14
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
     }
-    ]
-  }
-  
+  ]
+
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </div> 
-  )
-}
-
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part onePart={props.parts[0]}/>
-      <Part onePart={props.parts[1]}/>
-      <Part onePart={props.parts[2]}/>
+      {courses.map(course => <Course key={course.id} course={course} />
+      )}
     </div>
   )
 }
 
-const Total = (props) => {
-  return (
-    <p>Number of exercises {
-      props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}</p>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>{props.onePart.name} {props.onePart.exercises}</p>
-  )
-}
 
 export default App
