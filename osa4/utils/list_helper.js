f6ad2d13d2fe5f58e7baf37c69cd1f9e476decc3
jannepark <janneviljo@live.com>
2023-08-07 21:51:22 +0300

    4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -2,7 +2,17 @@ const dummy = (blogs) => {
     // ...
     return 1
   }
+const totalLikes = (blogs) => {
+    const sumOfLikes = (sum, item) => {
+        return sum + item.likes
+    }
+    return blogs.length === 0
+    ? 0
+    : blogs.reduce(sumOfLikes, 0)
+}
+  
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
