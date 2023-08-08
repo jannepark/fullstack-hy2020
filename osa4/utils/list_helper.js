733067086effc8cc0cd2dff5e7b8a910519beb85
jannepark <janneviljo@live.com>
2023-08-08 22:47:11 +0300

    4.7

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
     // ...
     return 1
@@ -10,9 +12,72 @@ const totalLikes = (blogs) => {
     ? 0
     : blogs.reduce(sumOfLikes, 0)
 }
+// const favoriteBlog = (blogs) => {
+//      const maxLikes = blogs.reduce((current, next) => {
+//         console.log(current);
+
+//         return Math.max(current, next.likes);
+//       }, 0);
+      
+//       console.log("Max Likes:", maxLikes);
+//       return maxLikes
+// }
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0 ) {
+        return null
+    }
+    const mostLikesBlog = blogs.reduce((currentMostLikesBlog, nextBlog) => {  
+      return nextBlog.likes > currentMostLikesBlog.likes ? nextBlog : currentMostLikesBlog;
+    }, blogs[0])
   
+    const { title, author, likes } = mostLikesBlog
+    const result = {  title, author, likes }
   
+    return result
+  }
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0 ) {
+        return null
+    }
+
+    const authorCount = lodash.countBy(blogs, 'author')
+    console.log(authorCount)
+    const mostBlogsAuthor = lodash.maxBy(Object.keys(authorCount), i => authorCount[i])
+
+    const mostBlogsCount = authorCount[mostBlogsAuthor]
+
+    return {
+        author: mostBlogsAuthor,
+        blogs: mostBlogsCount
+    }
+}
+const mostLikes = (blogs) => {
+    if (blogs.length === 0 ) {
+        return null
+    }
+    const blogsGroupedByAuthor = lodash.groupBy(blogs, 'author')
+
+    const authorsTotalLikes = lodash.mapValues(blogsGroupedByAuthor, authorBlogs =>
+        lodash.sumBy(authorBlogs, 'likes')
+      );
+      console.log(authorsTotalLikes)
+
+      const mostLikesAuthor = lodash.maxBy(Object.keys(authorsTotalLikes), author =>
+        authorsTotalLikes[author]
+      );
+      const mostLikesAmount = authorsTotalLikes[mostLikesAuthor]
+
+    return {
+        author:mostLikesAuthor,
+        likes:mostLikesAmount
+        }
+
+}
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
   }
+  
