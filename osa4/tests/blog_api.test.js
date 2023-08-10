44a19746ba750d66184086b49a5cb23fb9be6826
jannepark <janneviljo@live.com>
2023-08-10 21:01:02 +0300

    4.10,4.11

---
+++ osa4/tests/blog_api.test.js
@@ -0,0 +1,91 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+        {
+        "title": "pastaa2",
+        "author": "rop3",
+        "url": "www.goo3343gles.fi",
+        "likes": "14"
+      },
+      {
+        "title": "pastaa3",
+        "author": "rop2",
+        "url": "www.goo311343gles.fi",
+        "likes": "1"
+      },
+]
+beforeEach(async () => {
+    await Blog.deleteMany({})
+        let blogObject = new Blog(initialBlogs[0])
+        await blogObject.save()
+        blogObject = new Blog(initialBlogs[1])
+        await blogObject.save()
+    })
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+test('blogs id is id', async () => {
+    const response = await api.get('/api/blogs')
+
+    response.body.forEach(r => {
+        expect(r.id).toBeDefined()
+    })
+})
+
+
+test('a valid note can be added ', async () => {
+    const newBlog = {
+        "title": "pastaa322",
+        "author": "rop33",
+        "url": "www.goo311343gles.fi",
+        "likes": "4"
+      }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const response = await api.get('/api/blogs')
+  
+    const contents = response.body.map(r => r.title)
+  
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    expect(contents).toContain(
+      'pastaa322'
+    )
+  })
+  test('if default value of 0 is set if likes is missing', async () => {
+    const newBlog = {
+        "title": "pastaa322",
+        "author": "rop33",
+        "url": "www.goo311343gles.fi",
+        // "likes": "4"
+      }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    response.body.forEach(r => {
+      if(r.title === "pastaa322") {
+        expect(r.likes).toBe(0)
+      }
+  })
+  
+
+  })
+  afterAll(async () => {
+    await mongoose.connection.close()
+  })
