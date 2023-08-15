08133ef0b5e2824d0398a4630e62566a4879b2ee
jannepark <janneviljo@live.com>
2023-08-15 20:19:35 +0300

    4.23

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -8,6 +8,7 @@ const api = supertest(app)
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+
 const initialBlogs = [
         {
         "title": "Seitsemän veljestä",
@@ -21,25 +22,64 @@ const initialBlogs = [
         "url": "www.blogi2.fi",
         "likes": "3"
       },
+      {
+        "title": "Aku ankka",
+        "author": "Donrosa",
+        "url": "www.blogi3.fi",
+        "likes": "100"
+      }
 ]
 
 // Blogien testaukset rikki!!
 describe('when there is initially some blogs saved', () =>{
 
   beforeEach(async () => {
-      await Blog.deleteMany({})
-          let blogObject = new Blog(initialBlogs[0])
-          await blogObject.save()
-          blogObject = new Blog(initialBlogs[1])
-          await blogObject.save()
+    await Blog.deleteMany({})
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('123', 10)
+    const initialUser = new User({ username: 'root', name: 'Superuser', passwordHash })
+    await initialUser.save()
+    const user = {
+      username: "root",
+      password: "123"
+    }
+    
+    const login = await api
+      .post('/api/login')
+      .send(user)
+    const token = login.body.token
+      // await api
+      //   .post('/api/blogs')
+      //   .set('Authorization', `Bearer ${token}`)
+      //   .send(initialBlogs[0])
+      //   .expect(201)
+      //   .expect('Content-Type', /application\/json/)
+        for (const blog of initialBlogs) {
+          await api
+            .post("/api/blogs")
+            .set("Authorization", `Bearer ${token}`)
+            .send(blog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        }
+          // let blogObject = new Blog(initialBlogs[0])
+          // await blogObject.save()
+          // blogObject = new Blog(initialBlogs[1])
+          // await blogObject.save()
+
+
+
       })
 
   describe('returning of blogs', () => {
     test('blogs are returned as json', async () => {
-      await api
+      const blogs = await api
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
+
+      console.log(blogs.body)
     })
 
     test('blogs id is id', async () => {
@@ -52,14 +92,24 @@ describe('when there is initially some blogs saved', () =>{
   })
   describe('addition of a new blog', () => {
     test('a valid note can be added ', async () => {
+      const user = {
+        username: "root",
+        password: "123"
+      }
+      const login = await api
+        .post('/api/login')
+        .send(user)
+      const token = login.body.token
       const newBlog = {
-          "title": "Uusi_blogi",
-          "author": "Tuntematon",
-          "url": "www.blogi3.fi",
-          "likes": "1"
-        }
+        "title": "Uusi_blogi",
+        "author": "Tuntematon",
+        "url": "www.blogi3.fi",
+        "likes": "1"
+      }
+
       await api
         .post('/api/blogs')
+        .set('Authorization', `Bearer ${token} ` )
         .send(newBlog)
         .expect(201)
         .expect('Content-Type', /application\/json/)
@@ -71,14 +121,26 @@ describe('when there is initially some blogs saved', () =>{
       expect(contents).toContain('Uusi_blogi')
   })
     test('default value of 0 is set, if likes field is missing', async () => {
+      const user = {
+        username: "root",
+        password: "123"
+      }
+      const login = await api
+        .post('/api/login')
+        .send(user)
+        console.log(login.body)
+      const token = login.body.token
+console.log(token)
       const newBlog = {
         "title": "Uusi_blogi",
         "author": "Tuntematon",
         "url": "www.blogi3.fi",
         // "likes": "1"
       }
+
       await api
         .post('/api/blogs')
+        .set('Authorization', `Bearer ${token} ` )
         .send(newBlog)
         .expect(201)
         .expect('Content-Type', /application\/json/)
@@ -91,6 +153,14 @@ describe('when there is initially some blogs saved', () =>{
       })
     })
     test('400 bad response is sent if title or url missing ', async () => {
+      const user = {
+        username: "root",
+        password: "123"
+      }
+      const login = await api
+        .post('/api/login')
+        .send(user)
+      const token = login.body.token
       const newBlog = {
         // "title": "Uusi_blogi",
         "author": "Tuntematon",
@@ -99,6 +169,7 @@ describe('when there is initially some blogs saved', () =>{
       }
       await api
         .post('/api/blogs')
+        .set('Authorization', `Bearer ${token} ` )
         .send(newBlog)
         .expect(400)
     })
@@ -107,9 +178,18 @@ describe('when there is initially some blogs saved', () =>{
     test('deletetion of blog succeeds with status code 204 if id is valid', async () => {
       const responseInitial = await api.get('/api/blogs')
       const blogToDelete = responseInitial.body[1]
+      const user = {
+        username: "root",
+        password: "123"
+      }
+      const login = await api
+        .post('/api/login')
+        .send(user)
+      const token = login.body.token
 
       await api
       .delete(`/api/blogs/${blogToDelete.id}`)
+      .set('Authorization', `Bearer ${token}`)
       .expect(204)
 
       const responseAfterDel = await api.get('/api/blogs')
