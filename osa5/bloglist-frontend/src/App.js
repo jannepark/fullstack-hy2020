c3dc54a332eae237e2c69860cc73f14ca90a41b0
jannepark <janneviljo@live.com>
2023-08-17 13:43:33 +0300

    5.2

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -46,7 +46,21 @@ const App = () => {
     window.localStorage.clear()
     window.location.reload();
   }
+  const addblog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      tile: newTitle,
+      author: newAuthor,
+      url: newUrl,
+    }
 
+    blogService
+      .create(blogObject)
+        .then(returnedBlog => {
+        setNotes(notes.concat(returnedBlog))
+        setNewNote('')
+      })
+  }
   if (user === null) {
     return (
       <div>
@@ -83,9 +97,22 @@ const App = () => {
      {user.name} logged in 
      <button type="submit" onClick={handleLogout}>logout</button>
       </div>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <div>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    <Form onSubmit={addBlog}>
+      <div>
+        title
+        <input
+        type="text"
+        value={title}
+        name="title"
+        onChange={({ target })} => 
+
+      </div>
+    </Form>
     </div>
   )
 }
