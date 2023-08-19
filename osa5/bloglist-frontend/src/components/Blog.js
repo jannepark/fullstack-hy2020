c1707e3eba55cd303f703407d76786efcb636091
jannepark <janneviljo@live.com>
2023-08-19 16:12:51 +0300

    5.9

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,48 +1,63 @@
 import { useState } from 'react'
+import blogService from '../services/blogs'
 
 const Blog = ({ blog }) => {
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5
-  }
-  const [visible, setVisible] = useState(false)
 
+  const [visible, setVisible] = useState(false)
+  const [likes, setLikes] = useState(blog.likes)
   const toggleViewAll = () => {
     setVisible(!visible)
-    // console.log("nappi toimii")
+  }
+  const likeBlog = async () => {
+    try {
+      const newLikes = likes +1 
+
+      const blogObject = {
+        title: blog.title,
+        author: blog.author,
+        url: blog.url,
+        likes: newLikes,
+        user: blog.user
+      }
+      const response = await blogService.update(blog.id, blogObject)
+      setLikes(likes + 1)
+      console.log(likes)
+      return response
+    }
+    catch (error) {
+      console.log(error)
+    }
   }
 
-if(visible){
+  if (visible) {
+    return (
+      <div className="blogStyle">
+        <div>
+          <p>
+            {blog.title}
+            <button type="submit" onClick={toggleViewAll}>hide</button>
+          </p>
+          <p>{blog.author}</p>
+          <p>{blog.url}</p>
+          <p>{likes} 
+          <button type="submit" onClick={likeBlog}>Like</button>
+          </p>
+          <p>{blog.user.name}</p>
+        </div>
+      </div>
+    )
+  }
   return (
-    <div className="blogStyle">
-  <div>
-    <p>
-      {blog.title} 
-      <button type="submit" onClick={toggleViewAll}>hide</button>
-    </p>
-    <p>{blog.author}</p>
-    <p>{blog.url}</p>
-    <p>{blog.likes} <button type="submit" onClick={null}>Like</button></p>
-    <p>{blog.user.name}</p>
-  </div>
-  
-  </div>  
-  )
-}
-  return (
-  <>
-  <div className="blogStyle">
-    <p>
-      {blog.title} -  {blog.author}
-      <button type="submit" onClick={toggleViewAll}>view</button>
-    </p>
-   
-  </div>
-  
-  </>  
+    <>
+      <div className="blogStyle">
+        <p>
+          {blog.title} -  {blog.author}
+          <button type="submit" onClick={toggleViewAll}>view</button>
+        </p>
+
+      </div>
+
+    </>
   )
 }
 
