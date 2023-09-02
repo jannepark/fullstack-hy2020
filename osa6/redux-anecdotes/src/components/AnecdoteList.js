521f9c30db667b8b4c4348ae5c9b9d79a6e95f3d
jannepark <janneviljo@live.com>
2023-09-02 14:51:17 +0300

    6.9

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,12 +1,20 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { voteOf } from '../reducers/anecdoteReducer'
+import Filter from './Filter'
 
   const Anecdotes = () => {
     const dispatch = useDispatch()
-    const anecdotes = useSelector(state => state)
-    
+    const anecdotes = useSelector(state => {
+        if (state.filter === '') {
+            return state.anecdotes
+        }
+        return state.anecdotes.filter(item => 
+            item.content.toLowerCase().includes(state.filter.toLowerCase())
+          )
+  })
     return (
         <div>
+            <Filter />
         {anecdotes
         .sort(( i, j ) => j.votes - i.votes)
         .map(anecdote =>
--- osa6/redux-anecdotes/src/index.js
+++ osa6/redux-anecdotes/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import anecdotesReducer from './reducers/anecdoteReducer'
+import filterReducer from './reducers/fitlerReducer'
 
+const reducer = combineReducers({
+  anecdotes: anecdotesReducer,
+  filter: filterReducer
+})
 const store = createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
