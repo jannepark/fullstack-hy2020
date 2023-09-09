7bf117a99a85755e23c4d2ac7e627c00c6871db5
jannepark <janneviljo@live.com>
2023-09-09 20:14:22 +0300

    6.20 start

---
+++ osa6/query-anecdotes/src/requests/requests.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+
+export const getAnecdotes = () => {
+    return axios.get('http://localhost:3001/anecdotes')
+      .then(res => { 
+        return res.data;
+      });
+  }
+  
