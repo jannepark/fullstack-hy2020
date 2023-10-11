ad9e231147671588a117c10b03982b0c50e0a874
jannepark <janneviljo@live.com>
2023-10-11 22:10:07 +0300

    8.12

--- osa8/library-frontend/src/components/Authors.js
+++ osa8/library-frontend/src/components/Authors.js
@@ -1,8 +1,20 @@
-import { gql, useQuery } from '@apollo/client'
+import { gql, useQuery, useMutation } from '@apollo/client'
+import { useState } from 'react'
+import Select from 'react-select'
 
 const ALL_AUTHORS = gql`
   query {
     allAuthors {
+      name
+      born
+      id
+      bookCount
+    }
+  }
+`
+const EDIT_AUTHOR = gql`
+  mutation editAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
       name
       born
       id
@@ -11,8 +23,16 @@ const ALL_AUTHORS = gql`
 `
 
 const Authors = (props) => {
+  const [born, setborn] = useState('')
+  const [name, setName] = useState('')
   const result = useQuery(ALL_AUTHORS, {
     pollInterval: 5000,
+  })
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    onError: (error) => {
+      const messages = error.graphQLErrors.map((e) => e.message).join('\n')
+      console.log(messages)
+    },
   })
   if (!props.show) {
     return null
@@ -21,9 +41,26 @@ const Authors = (props) => {
     return <div>loading...</div>
   }
   const authors = result.data.allAuthors
+  const options = authors.map((author) => ({
+    value: author.name,
+    label: author.name,
+  }))
+  const submit = async (event) => {
+    event.preventDefault()
+
+    editAuthor({
+      variables: {
+        name: name.value,
+        setBornTo: parseInt(born, 10),
+      },
+    })
+    console.log('add book...')
+    setName('')
+    setborn('')
+  }
   return (
     <div>
-      <h2>authors</h2>
+      <h2>Authors</h2>
       <table>
         <tbody>
           <tr>
@@ -40,6 +77,21 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+      <h2>Set birthyear</h2>
+      <form onSubmit={submit}>
+        <div className="App">
+          Name
+          <Select defaultValue={name} onChange={setName} options={options} />
+        </div>
+        <div>
+          Born
+          <input
+            value={born}
+            onChange={({ target }) => setborn(target.value)}
+          />
+        </div>
+        <button type="submit">Update author</button>
+      </form>
     </div>
   )
 }
