ae7065b1d590d3e1b1827f2640a8a9a7bcfb9616
jannepark <janneviljo@live.com>
2023-10-25 23:11:16 +0300

    9.3

---
+++ osa9/9_1-9_7/bmicalculator.ts
@@ -0,0 +1,22 @@
+const calculateBmi = (height: number, weight: number) => {
+  const bmi = weight / ((height / 100) * (height / 100));
+  let category;
+  console.log(bmi);
+  if (bmi < 18.5) {
+    category = 'Underweight';
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    category = 'Normal (healthy weight)';
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    category = 'Overweight';
+  } else if (bmi >= 30) {
+    category = 'Obese';
+  } else {
+    category = 'Invalid BMI'; // for negative or undefined values
+  }
+
+  return category;
+};
+
+const height: number = Number(process.argv[2]);
+const weight: number = Number(process.argv[3]);
+console.log(calculateBmi(height, weight));
---
+++ osa9/9_1-9_7/exerciseCalculator.ts
@@ -0,0 +1,75 @@
+interface exerciseResult {
+  periodLength: number;
+  trainingDays: number;
+  target: number;
+  average: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+}
+
+interface Args {
+  argsTarget: number;
+  argsHours: number[];
+}
+
+const parseArguments = (args: string[]): Args => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+
+  const argsHours: number[] = [];
+
+  for (let i = 3; i < args.length; i++) {
+    if (isNaN(Number(args[i]))) {
+      throw new Error('args not numbers');
+    } else {
+      argsHours.push(Number(args[i]));
+    }
+  }
+
+  return {
+    argsTarget: Number(args[2]),
+    argsHours: argsHours,
+  };
+};
+
+const exerciseCalculator = (
+  trainingHours: number[],
+  targetHours: number
+): exerciseResult => {
+  const periodLength = trainingHours.length;
+  const trainingDays = trainingHours.filter(
+    (trainingHours) => trainingHours > 0
+  ).length;
+  const target = targetHours;
+  const average =
+    trainingHours.reduce((acc, current) => acc + current, 0) /
+    trainingHours.length;
+  const success = average >= 2 ? true : false;
+  const rating = average >= 3 ? 3 : average >= 2 && average < 3 ? 2 : 1;
+  const ratingDescription =
+    rating == 3
+      ? 'Good!'
+      : rating == 2
+      ? 'Not too bad but could be better'
+      : 'You failed';
+  return {
+    periodLength: periodLength,
+    trainingDays: trainingDays,
+    success: success,
+    rating: rating,
+    ratingDescription: ratingDescription,
+    target: target,
+    average: average,
+  };
+};
+
+try {
+  const { argsTarget, argsHours } = parseArguments(process.argv);
+  console.log(exerciseCalculator(argsHours, argsTarget));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
