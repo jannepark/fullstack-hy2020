2d9b9723ed41b9d7ed4675ff3f6621a740db65e7
jannepark <janneviljo@live.com>
2023-11-08 20:06:32 +0200

    9.14

---
+++ osa9/course-information/src/components/Content.tsx
@@ -0,0 +1,18 @@
+import { ContentProps } from '../types';
+
+const Content = (props: ContentProps) => {
+  return (
+    <div>
+      <p>
+        {props.courseParts[0].name} {props.courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {props.courseParts[1].name} {props.courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {props.courseParts[2].name} {props.courseParts[2].exerciseCount}
+      </p>
+    </div>
+  );
+};
+export default Content;
